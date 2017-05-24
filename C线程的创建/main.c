6e9818adeabcd21a16402072703d9d7a928cd025
SHICHUAN <15180796292@163.com>
2017-05-24 12:29:15 +0800

    Initial Commit

---
+++ C线程的创建/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  C线程的创建
+//
+//  Created by SHICHUAN on 2017/5/24.
+//  Copyright © 2017年 SHICHUAN. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
